fecbca7e8a028b5983540f3c7073a6cf6b17d8ac
cafeed28 <62172116+cafeed28@users.noreply.github.com>
2025-07-27 13:58:11 +0500

    agp and sdk update

--- app/src/main/java/com/cafeed28/omori/MainActivity.java
+++ app/src/main/java/com/cafeed28/omori/MainActivity.java
@@ -2,10 +2,15 @@ package com.cafeed28.omori;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.preference.PreferenceManager;
 
 import java.io.IOException;
@@ -24,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button playButton = findViewById(R.id.button_play);
+
+        ViewCompat.setOnApplyWindowInsetsListener(playButton, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            mlp.bottomMargin = insets.bottom;
+            v.setLayoutParams(mlp);
+
+            return WindowInsetsCompat.CONSUMED;
+        });
+
         var fragment = new SettingsFragment();
 
         getSupportFragmentManager()
@@ -31,10 +47,19 @@ public class MainActivity extends AppCompatActivity {
                 .replace(R.id.frame_preferences, fragment)
                 .commit();
 
-        Button playButton = findViewById(R.id.button_play);
-
         fragment.setOnPreferencesUpdateListener((preferences) -> {
             playButton.setEnabled(fragment.canPlay(this, preferences));
+        });
+
+        View framePreferences = findViewById(R.id.frame_preferences);
+
+        ViewCompat.setOnApplyWindowInsetsListener(framePreferences, (v, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            mlp.topMargin = insets.top;
+            v.setLayoutParams(mlp);
+
+            return WindowInsetsCompat.CONSUMED;
         });
 
         playButton.setEnabled(fragment.canPlay(this, PreferenceManager.getDefaultSharedPreferences(this)));
